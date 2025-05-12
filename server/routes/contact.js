2eae210d475766d7eb8acb22abd5013bb9ed76d3
Shivam Prakash <168464660+shivamqums7482@users.noreply.github.com>
2025-05-13 02:26:08 +0530

    Update contact.js

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -4,7 +4,6 @@ const Contact = require('../models/Contact'); // Import the Contact model
 
 // POST route to save contact form data
 router.post('/', async (req, res) => {
-    res.send("hit api");
     try {
         // Destructure the incoming request body
         const { name, email, phone, subject, message } = req.body;
@@ -20,6 +19,7 @@ router.post('/', async (req, res) => {
 
         // Save the document in the database
         await newContact.save();
+        res.send("data saved");
 
         // Send a success response
         res.status(201).json({ message: 'Message saved successfully!' });
